b9849d8b901ec2e71aff49880fc8957ea5647191
nayavdoschak <nayavdoschak@nvutc.rosatomtech.ru>
2025-08-19 09:54:29 +0300

    5.1 - Router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
 import { TrpcProvider } from './lib/trps.tsx';
 import { AllIdeasPage } from './pages/Ideas';
+import { ViewIdeaPage } from './pages/ViewIdeaPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes';
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
-  )
-}
+  );
+};
--- webapp/src/pages/Ideas/index.tsx
+++ webapp/src/pages/Ideas/index.tsx
@@ -1,13 +1,13 @@
 import { trpc } from '../../lib/trps';
+import { Link } from 'react-router-dom';
+import { getViewIdeaRoute } from '../../lib/routes';
 
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery();
 
-  if (isLoading || isFetching)
-    return <span>Loading...</span>;
+  if (isLoading || isFetching) return <span>Loading...</span>;
 
-  if (isError)
-    return <span>Error: {error.message}</span>;
+  if (isError) return <span>Error: {error.message}</span>;
 
   return (
     <div>
@@ -15,7 +15,9 @@ export const AllIdeasPage = () => {
       <div>
         {data!.ideas.map((idea) => (
           <div key={idea.nick}>
-            <h2>{idea.name}</h2>
+            <h2>
+              <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+            </h2>
             <p>{idea.description}</p>
           </div>
         ))}
