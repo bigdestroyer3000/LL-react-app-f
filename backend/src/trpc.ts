c29659063c111028992bdd9b7e096e6482fa764d
nayavdoschak <nayavdoschak@nvutc.rosatomtech.ru>
2025-08-20 10:48:00 +0300

    6.1 - lodash

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,18 +1,18 @@
 import { initTRPC } from '@trpc/server';
+import _ from 'lodash'
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'idea 1', description: 'Description of idea 1...' },
-  { nick: 'cool-idea-nick-2', name: 'idea 2', description: 'Description of idea 2...' },
-  { nick: 'cool-idea-nick-3', name: 'idea 3', description: 'Description of idea 3...' },
-  { nick: 'cool-idea-nick-4', name: 'idea 4', description: 'Description of idea 4...' },
-  { nick: 'cool-idea-nick-5', name: 'idea 5', description: 'Description of idea 5...' },
-];
+const ideas = _.times(100, (i)=> ({
+  nick: `cool-idea-nick-${i}`,
+  name: `idea ${i}`,
+  description: `Description of idea ${i}...`,
+  text: _.times(100, (j) => `<p>Text paragraph ${j} of idea ${i}...</p>`).join('')
+}));
 
 const trpc = initTRPC.create();
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
-    return { ideas };
+    return { ideas: ideas.map((idea)=>_.pick(idea, ['nick','name','description'])) };
   }),
 });
 
