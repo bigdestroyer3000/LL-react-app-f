de41a1f0c6442a4e261cccfc37bc3bfb67f48bd8
nayavdoschak <nayavdoschak@nvutc.rosatomtech.ru>
2025-08-24 16:28:12 +0300

    7.1 - Layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,14 +3,17 @@ import { AllIdeasPage } from './pages/Ideas';
 import { ViewIdeaPage } from './pages/ViewIdeaPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes';
+import { Layout } from './components/Layout';
 
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          <Route element={<Layout />}>
+            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </TrpcProvider>
