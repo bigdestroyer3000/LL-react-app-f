2f4bfa945406410cac76e41bc8054f00a7ee7020
nayavdoschak <nayavdoschak@nvutc.rosatomtech.ru>
2025-09-11 09:49:15 +0300

    8.1 - Form React

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,25 +1,79 @@
-//import { useParams } from 'react-router-dom';
-//import { type ViewIdeaRouteParams } from '../../lib/routes';
-//import { trpc } from '../../lib/trps';
-import css from './index.module.scss';
+import { useState } from 'react';
+import { Segment } from '../../components/Segment';
 
 export const NewIdeaPage = () => {
-  //const { ideaNick } = useParams() as ViewIdeaRouteParams;
-
-  //const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({
-  //  ideaNick,
-  //});
-
-  //if (isLoading || isFetching) return <span>Loading...</span>;
-
-  //if (isError) return <span>Error: {error.message}</span>;
-
-  //if (!data!.idea) return <span>Idea not found</span>;
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  });
 
   return (
-    <div>
-      <h1 className={css.title}>New Idea</h1>
-      <div>Form will be here...</div>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.info('Submitted', state);
+        }}
+      >
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => {
+              setState({ ...state, name: e.target.value });
+            }}
+            value={state.name}
+            name="name"
+            id="name"
+          />
+        </div>
+
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="nick">Nick</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => {
+              setState({ ...state, nick: e.target.value });
+            }}
+            value={state.nick}
+            name="nick"
+            id="nick"
+          />
+        </div>
+
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="description">Description</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => {
+              setState({ ...state, description: e.target.value });
+            }}
+            value={state.description}
+            name="description"
+            id="description"
+          />
+        </div>
+
+        <div style={{ marginBottom: 10 }}>
+          <label htmlFor="text">Text</label>
+          <br />
+          <textarea
+            onChange={(e) => {
+              setState({ ...state, text: e.target.value });
+            }}
+            value={state.text}
+            name="text"
+            id="text"
+          />
+        </div>
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
   );
 };
